88a48c9f6a8d4d0a3173fe1f33e8d35b4264a0e5
Carlos A. Cruz <carlos.a.cruz@gmail.com>
2026-09-13 14:44:28 -0400

    Add comments on basic syntax

--- sandbox/general.f90
+++ sandbox/general.f90
@@ -1,7 +1,10 @@
 program program_name
   !use some_module
   implicit none
-  
+
+  ! This is a comment - the line is ignored by the compiler
+  ! Comments can start in any column
+
   ! Declarations
   integer :: count
   real :: value
@@ -9,5 +12,13 @@ program program_name
   ! Executable statements
   value = 3.0
   print *, value
-  
+ 
+  ! The statement continues seamlessly onto the next line
+  large_result = variable_one + variable_two + variable_three &
+             + variable_four + variable_five  
+
+  ! Fortran allows lines up to 132 characters but...
+  print *, "This is a very long error message that needs to be broken up &
+         &so that it does not exceed the line length limit of the compiler."
+
 end program program_name
